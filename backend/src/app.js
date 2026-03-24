cb373efe4c712860efe461714c086fb85b5ee487
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-03-24 10:54:14 +0530

    post are done

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 // ✅ Correct path (VERY IMPORTANT)
 const __dirname2 = path.resolve()
 
-app.use(express.static(path.join(__dirname2, "../frontend/dist")))
+app.use(express.static(path.join(__dirname2, "dist")))
 
 // ✅ Routes
 const postRouter = require("./routes/post.routes")
@@ -30,7 +30,7 @@ app.use("/api/user", userRouter)
 
 // ✅ Catch-all (React routing fix)
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname2, "../frontend/dist", "index.html"))
+    res.sendFile(path.join(__dirname2, "dist", "index.html"))
 })
 
 module.exports = app
